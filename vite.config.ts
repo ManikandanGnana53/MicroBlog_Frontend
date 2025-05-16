90e14e8de1a3793fbbcd589683b54f3aa6f71980
ManikandanGnanaGurusamy <manikandan.gnana@hcltech.com>
2025-05-16 16:46:08 +0530

    Setup for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
+  base:'MicroBlog_Frontend',
   plugins: [react(),tailwindcss()],
 })
